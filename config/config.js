788802290f3afa2755897e853d23f63359a61b86
Destinum <david.hoglund.96@gmail.com>
2022-07-31 02:39:58 +0200

    Emperoc custom sprite

--- config/config.js
+++ config/config.js
@@ -33,7 +33,7 @@ Config.customcolors = {
 	'zarel': 'aeo'
 };
 /*** Begin automatically generated configuration ***/
-Config.version = "0.11.2 (7802e102)";
+Config.version = "0.11.2 (60d2214d)";
 
 Config.routes = {
 	root: 'pokemonshowdown.com',
