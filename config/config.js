aba967acd0cef09af954470d0d285f5a471db1d5
Destinum <david.hoglund.96@gmail.com>
2022-08-01 05:28:38 +0200

    Move animations

--- config/config.js
+++ config/config.js
@@ -33,7 +33,7 @@ Config.customcolors = {
 	'zarel': 'aeo'
 };
 /*** Begin automatically generated configuration ***/
-Config.version = "0.11.2 (60d2214d)";
+Config.version = "0.11.2 (f51e04d3)";
 
 Config.routes = {
 	root: 'pokemonshowdown.com',
