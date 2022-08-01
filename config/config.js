fca59bb198305840d1d0ed773bb9d93846c82fba
Destinum <david.hoglund.96@gmail.com>
2022-08-02 01:22:24 +0200

    changed gitignore

--- config/config.js
+++ config/config.js
@@ -33,7 +33,7 @@ Config.customcolors = {
 	'zarel': 'aeo'
 };
 /*** Begin automatically generated configuration ***/
-Config.version = "0.11.2 (aba967ac)";
+Config.version = "0.11.2 (9f882900)";
 
 Config.routes = {
 	root: 'pokemonshowdown.com',
