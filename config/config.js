0e93bdad2c20ca19680ce647793c8f91e6972095
Destinum <david.hoglund.96@gmail.com>
2022-08-04 03:06:33 +0200

    Lots of Sprites

--- config/config.js
+++ config/config.js
@@ -33,7 +33,7 @@ Config.customcolors = {
 	'zarel': 'aeo'
 };
 /*** Begin automatically generated configuration ***/
-Config.version = "0.11.2 (9f882900)";
+Config.version = "0.11.2 (fca59bb1)";
 
 Config.routes = {
 	root: 'pokemonshowdown.com',
